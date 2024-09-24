b3509055fc252b7d8cff621fb4eaa2638f82581d
Hayleigh Thompson <me@hayleigh.dev>
2024-09-24 23:31:49 +0100

    :bug: Fixed a bug where the wrong file was imported in the component entry.

--- priv/template/component-entry.mjs
+++ priv/template/component-entry.mjs
@@ -1,4 +1,4 @@
-import { register } from '../dev/javascript/lustre/client-component.ffi.mjs';
+import { make_lustre_client_component } from '../dev/javascript/lustre/lustre.ffi.mjs';
 import { name, {component_name} as component } from '../dev/javascript/{app_name}/{module_path}.mjs';
 
-register(component(), name);
+make_lustre_client_component(component(), name);
