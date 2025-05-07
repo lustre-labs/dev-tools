183f2d454f85e27f610f12aca9ad6a56809d65ac
Hayleigh Thompson <me@hayleigh.dev>
2025-05-07 22:20:08 +0200

    :bug: Fixed a bug where component template imported something from the wrong path.

--- priv/template/component-entry.mjs
+++ priv/template/component-entry.mjs
@@ -1,4 +1,4 @@
-import { make_lustre_client_component } from '../dev/javascript/lustre/lustre.ffi.mjs';
+import { make_component } from '../dev/javascript/lustre/lustre/runtime/client/component.ffi.mjs';
 import { name, {component_name} as component } from '../dev/javascript/{app_name}/{module_path}.mjs';
 
-make_lustre_client_component(component(), name);
+make_component(component(), name);
