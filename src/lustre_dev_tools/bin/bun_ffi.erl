6d4a369aca705bfc5a41b1bc33c562a508a37b52
Hayleigh Thompson <me@hayleigh.dev>
2026-01-04 21:18:54 +0100

    :bug: Fixed a bug where matching on zip:unzip did not account for differences in platform.

--- src/lustre_dev_tools/bin/bun_ffi.erl
+++ src/lustre_dev_tools/bin/bun_ffi.erl
@@ -2,9 +2,14 @@
 
 -export([extract/2]).
 
-%
+
 extract(Archive, To) ->
     case zip:unzip(Archive, [{cwd, unicode:characters_to_list(To)}]) of
+        % Some systems will only report the actual extracted file, like
+        % "./.lustre/bin/bun-linux-x64/bun"
+        {ok, [Bin]} ->
+            {ok, list_to_binary(Bin)};
+        % Others (like macOS) will report both the directory and the file
         {ok, [_, Bin]} ->
             {ok, list_to_binary(Bin)};
         {error, _} ->
